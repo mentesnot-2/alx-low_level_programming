36c630649cb568bb3a20e16bcf4f41532443a951
mentesnot-2 <sibhatmentesnot@gmail.com>
2022-10-13 21:28:08 -0700

    my first c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always
+ * Return: Always 0
  */
 int main(void)
 {
@@ -10,3 +10,4 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof((long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof((long long int));        printf("Size of a float: %i byte(s)\n", sizeof((float));
 	return (0);
+}
